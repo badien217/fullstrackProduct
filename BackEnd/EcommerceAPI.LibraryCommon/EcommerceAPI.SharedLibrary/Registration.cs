06fefcef9c8e989d374b66deff840a77f6ef7537
vubadien <badien2172004@gmail.com>
2024-08-16 16:36:26 +0700

    .

--- BackEnd/EcommerceAPI.LibraryCommon/EcommerceAPI.SharedLibrary/Registration.cs
+++ BackEnd/EcommerceAPI.LibraryCommon/EcommerceAPI.SharedLibrary/Registration.cs
@@ -16,12 +16,12 @@ namespace EcommerceAPI.SharedLibrary
             var assembly = Assembly.GetExecutingAssembly();
             services.AddTransient<ExceptionMiddleware>();
             services.AddRulesFromAssemblyContaining(assembly, typeof(BaseRules));
-            services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(assembly));
+          ;
             //services.Configure<SendMailCommandAuthsSettings>(configuration.GetSection("MailSetting"));
             services.AddValidatorsFromAssembly(assembly);
             ValidatorOptions.Global.LanguageManager.Culture = new CultureInfo("tr");
             services.AddTransient(typeof(IPipelineBehavior<,>), typeof(FluentValidationBehevior<,>));
-            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(RedisCacheBehevior<,>));
+            //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(RedisCacheBehevior<,>));
         }
         private static IServiceCollection AddRulesFromAssemblyContaining(
             this IServiceCollection services,
