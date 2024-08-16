c6779456c21f23ae76f8828c7aec89bf98103df6
vubadien <badien2172004@gmail.com>
2024-08-16 16:40:49 +0700

    final

---
+++ BackEnd/EcommerceApi.Order/Order.Infratructure/Registration.cs
@@ -0,0 +1,26 @@
+﻿using EcommerceAPI.SharedLibrary.Interfaces.Reponsitories;
+using EcommerceAPI.SharedLibrary.Interfaces.UnitOfWorks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Order.Infratructure.context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Order.Infratructure
+{
+    public static class Registration
+    {
+        public static void AddInfratructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AddDbContext>(opt =>
+            opt.UseSqlServer(configuration.GetConnectionString("DefaultConnect")));
+            //services.AddScoped<IUnitOfWork, UnitOfWorkServices>();
+            //services.AddScoped(typeof(IReponsitory<>), typeof(ReponsitoryServices<>));
+
+        }
+    }
+}
