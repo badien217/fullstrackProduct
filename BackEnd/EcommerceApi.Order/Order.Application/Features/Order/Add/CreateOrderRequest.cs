27abd53b6980f7c32ad432b3a79f774810db4362
dien <badien2172004@gmail.com>
2024-08-19 17:45:56 +0700

    ass

---
+++ BackEnd/EcommerceApi.Order/Order.Application/Features/Order/Add/CreateOrderRequest.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Order.Application.Features.Order.Add
+{
+    public class CreateOrderRequest : IRequest<Unit>
+    {
+        public int ProfileId { get; set; }
+        public string name { get; set; }
+        public string address { get; set; }
+        public string phoneNumber { get; set; }
+        public decimal amount { get; set; }
+        public IList<int> ProductId { get; set; }
+        public bool status { get; set; }
+        public int OrderId { get; set; }
+        public decimal price { get; set; }
+    }
+}
